4c72da4991a555a9e6f02f2fc0dadc7761776fdd
Jarrod Seccombe <jarrod@cynical.software>
2020-12-28 13:02:22 -0500

    convert status module to use Prisma

--- api/src/status/status.controller.ts
+++ api/src/status/status.controller.ts
@@ -1,14 +1,13 @@
 import { Controller, Get } from '@nestjs/common';
-
-import { Status } from './status.model';
 import { StatusService } from './status.service';
+import { Statuses } from '@prisma/client';
 
 @Controller('statuses')
 export class StatusController {
   constructor(private statusService: StatusService) {}
 
   @Get()
-  public async getAll(): Promise<Status[]> {
+  public async getAll(): Promise<Statuses[]> {
     return this.statusService.findAll();
   }
 }
--- api/src/status/status.module.ts
+++ api/src/status/status.module.ts
@@ -3,10 +3,11 @@ import { SequelizeModule } from '@nestjs/sequelize';
 import { Status } from './status.model';
 import { StatusController } from './status.controller';
 import { StatusService } from './status.service';
+import { PrismaService } from '../services/prisma.service';
 
 @Module({
   imports: [SequelizeModule.forFeature([Status])],
   controllers: [StatusController],
-  providers: [StatusService],
+  providers: [StatusService, PrismaService],
 })
 export class StatusModule {}
