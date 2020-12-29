41563d3828e5c706e8ab785da2ca7773b2ecd556
Jarrod Seccombe <jarrod@cynical.software>
2020-12-28 19:59:01 -0500

    [chore] Update dependencies (#134)
    * update dependencies
    
    * Remove dependency on @nest-middlewares
    
    Due to an existing bug where @nest-middlewares/morgan won't build due to missing template types,
    remove the existing dependency and write our own middleware wrapper around the standard morgan package.
    
    * add custom logging middleware
    
    * update pg

--- api/src/app/app.module.ts
+++ api/src/app/app.module.ts
@@ -1,34 +1,34 @@
 import * as dbConfig from '../../config/config.json';
 
-import { MiddlewareConsumer, Module } from '@nestjs/common';
+import {MiddlewareConsumer, Module} from '@nestjs/common';
 
-import { APP_FILTER } from '@nestjs/core';
-import { AllExceptionsFilter } from '../filters/http-exception.filter';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
-import { Circuit } from '../circuit/circuit.model';
-import { CircuitModule } from '../circuit/circuit.module';
-import { Constructor } from '../constructor/constructor.model';
-import { ConstructorModule } from '../constructor/constructor.module';
-import { ConstructorResult } from '../constructor/constructor-result.model';
-import { ConstructorStanding } from '../constructor/constructor-standing.model';
-import { Dialect } from 'sequelize/types';
-import { Driver } from '../driver/driver.model';
-import { DriverModule } from '../driver/driver.module';
-import { DriverStanding } from '../driver/driver-standing.model';
-import { LapTime } from '../timing/lap-time.model';
-import { MorganMiddleware } from '@nest-middlewares/morgan';
-import { PitStop } from '../timing/pit-stop.model';
-import { Qualification } from '../timing/qualification.model';
-import { Race } from '../race/race.model';
-import { RaceModule } from '../race/race.module';
-import { RaceResult } from '../race/race-result.model';
-import { Season } from '../season/season.model';
-import { SeasonModule } from '../season/season.module';
-import { SequelizeModule } from '@nestjs/sequelize';
-import { Status } from '../status/status.model';
-import { StatusModule } from '../status/status.module';
-import { TimingModule } from '../timing/timing.module';
+import {APP_FILTER} from '@nestjs/core';
+import {AllExceptionsFilter} from '../filters/http-exception.filter';
+import {AppController} from './app.controller';
+import {AppService} from './app.service';
+import {Circuit} from '../circuit/circuit.model';
+import {CircuitModule} from '../circuit/circuit.module';
+import {Constructor} from '../constructor/constructor.model';
+import {ConstructorModule} from '../constructor/constructor.module';
+import {ConstructorResult} from '../constructor/constructor-result.model';
+import {ConstructorStanding} from '../constructor/constructor-standing.model';
+import {Dialect} from 'sequelize/types';
+import {Driver} from '../driver/driver.model';
+import {DriverModule} from '../driver/driver.module';
+import {DriverStanding} from '../driver/driver-standing.model';
+import {LapTime} from '../timing/lap-time.model';
+import {PitStop} from '../timing/pit-stop.model';
+import {Qualification} from '../timing/qualification.model';
+import {Race} from '../race/race.model';
+import {RaceModule} from '../race/race.module';
+import {RaceResult} from '../race/race-result.model';
+import {Season} from '../season/season.model';
+import {SeasonModule} from '../season/season.module';
+import {SequelizeModule} from '@nestjs/sequelize';
+import {Status} from '../status/status.model';
+import {StatusModule} from '../status/status.module';
+import {TimingModule} from '../timing/timing.module';
+import {LoggerMiddleware} from '../middlewares/logger.middleware';
 
 @Module({
   imports: [
@@ -73,7 +73,6 @@ import { TimingModule } from '../timing/timing.module';
 })
 export class AppModule {
   configure(consumer: MiddlewareConsumer): void {
-    MorganMiddleware.configure('dev');
-    consumer.apply(MorganMiddleware).forRoutes('/');
+    consumer.apply(LoggerMiddleware).forRoutes('/');
   }
 }
