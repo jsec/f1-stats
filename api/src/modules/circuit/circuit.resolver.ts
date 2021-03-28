a1642877695a1f73de1d7ebc8852407277a3280c
Jarrod Seccombe <jarrod@cynical.software>
2021-03-28 10:08:01 -0400

    add circuit graphql infrastructure

---
+++ api/src/modules/circuit/circuit.resolver.ts
@@ -0,0 +1,18 @@
+import { NotFoundException } from '@nestjs/common';
+import { Args, Query, Resolver } from '@nestjs/graphql';
+
+import { CircuitModel } from './circuit.model';
+import { CircuitService } from './circuit.service';
+
+@Resolver(CircuitModel)
+export class CircuitResolver {
+  constructor(private circuitService: CircuitService) {}
+
+  @Query(returns => CircuitModel)
+  public async circuit(@Args('id') id: number): Promise<CircuitModel> {
+    const circuit = await this.circuitService.findById(id);
+    if (!circuit) throw new NotFoundException(id);
+
+    return circuit;
+  }
+}
