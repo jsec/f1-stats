cb1cf8429b5093f256eeb2c8b87c7831b0d20527
Jarrod Seccombe <jsec@hey.com>
2021-11-25 14:00:46 -0500

    add objection-nestjs package

--- libs/data/src/lib/data.module.ts
+++ libs/data/src/lib/data.module.ts
@@ -1,13 +1,12 @@
 import { Module } from '@nestjs/common';
-/* import { knex } from 'knex';
-import { knexSnakeCaseMappers, Model } from 'objection';
+import { ObjectionModule } from '@willsoto/nestjs-objection';
+import { knexSnakeCaseMappers } from 'objection';
 import {
   Circuit,
   Constructor,
   ConstructorResult,
   ConstructorStanding,
   Driver,
-  DriverStanding,
   LapTime,
   PitStop,
   Qualifying,
@@ -15,52 +14,35 @@ import {
   RaceResult,
   Season,
   Status
-} from './models'; */
-
-/* const models = [
-  Circuit,
-  Constructor,
-  ConstructorResult,
-  ConstructorStanding,
-  Driver,
-  DriverStanding,
-  LapTime,
-  PitStop,
-  Qualifying,
-  Race,
-  RaceResult,
-  Season,
-  Status
-]; */
-
-/* const modelProviders = models.map(model => {
-  return {
-    provide: model.name,
-    useValue: model
-  };
-});
-
-const providers = [
-  ...modelProviders,
-  {
-    provide: 'KnexConnection',
-    useFactory: async () => {
-      const conn = knex({
-        client: 'pg',
-        connection: process.env.DATABASE_URL,
-        debug: process.env.KNEX_DEBUG === 'true',
-        ...knexSnakeCaseMappers()
-      });
-
-      Model.knex(conn);
-      return conn;
-    }
-  }
-]; */
+} from './models';
+import { BaseModel } from './models/base.model';
 
 @Module({
-  controllers: [],
-  providers: [],
-  exports: []
+  imports: [
+    ObjectionModule.register({
+      Model: BaseModel,
+      config: {
+        client: 'pg',
+        connection: process.env.DATABASE_URL,
+        ...knexSnakeCaseMappers
+      }
+    }),
+
+    ObjectionModule.forFeature([
+      Circuit,
+      Constructor,
+      ConstructorResult,
+      ConstructorStanding,
+      Driver,
+      LapTime,
+      PitStop,
+      Qualifying,
+      RaceResult,
+      Race,
+      Season,
+      Status
+    ])
+  ],
+  exports: [ObjectionModule]
 })
 export class DataModule {}
