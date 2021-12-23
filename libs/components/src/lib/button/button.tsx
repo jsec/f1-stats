1393930f1c994a7ec22b78df4841b55c4efbc000
Jarrod Seccombe <jsec@hey.com>
2021-12-22 22:24:51 -0500

    build(ui): add base web application

--- libs/components/src/lib/button/button.tsx
+++ libs/components/src/lib/button/button.tsx
@@ -5,9 +5,12 @@ export interface ButtonProps {}
 
 export function Button(props: ButtonProps) {
   return (
-    <div>
-      <h1>Welcome to Button!</h1>
-    </div>
+    <button
+      type="button"
+      className="inline-flex items-center px-2.5 py-1.5 border border-transparent text-xs font-medium rounded text-indigo-700 bg-indigo-100 hover:bg-indigo-200 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500"
+    >
+      Button text
+    </button>
   );
 }
 
